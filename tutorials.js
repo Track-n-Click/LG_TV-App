4dd2869d1620a88d6a3fd381823abf2e33c99924
Darshana Thamara <darshanathamara143@gmail.com>
2024-08-15 22:55:19 +0530

    synamedia

---
+++ tutorials.js
@@ -0,0 +1,146 @@
+let selected = 0;
+let totalCells = 9; // Assuming 3 rows and 3 columns (9 cells)
+
+window.addEventListener("load", async () => {
+  try {
+    await senza.init();
+
+    updateVideos();
+
+    senza.remotePlayer.addEventListener("ended", () => {
+      senza.lifecycle.moveToForeground();
+    });
+
+    senza.uiReady();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function updateVideos() {
+  videos.forEach((video, index) => {
+    let rowClass = index === selected ? "row selected" : "row";
+    table.innerHTML += `<div class="${rowClass}">
+      <div class="thumb" onclick="redirect()">
+        <img src="${video.thumb}">
+      </div>
+      <div class="text">
+        <div class="title">${video.title}</div>
+        
+      </div>
+    </div>`;
+  });
+}
+
+function redirect() {
+  window.open("https://www.ayozat.com/", "_blank");
+}
+
+function redirectToNewPage() {
+  window.location.href = "newPage.html";
+}
+
+function backToMain() {
+  window.location.href = "index.html";
+}
+// <div class="description">${video.description}</div>
+
+document.addEventListener("keydown", async function (event) {
+  switch (event.key) {
+    case "Enter":
+      toggleVideo();
+      // redirect();
+      // redirectToNewPage();
+      break;
+    case "Escape":
+      // toggleVideo();
+      backToMain();
+      break;
+    case "ArrowUp":
+      up();
+      break;
+    case "ArrowDown":
+      down();
+      break;
+    case "ArrowLeft":
+      left();
+      break;
+    case "ArrowRight":
+      right();
+      break;
+    default:
+      return;
+  }
+  event.preventDefault();
+});
+
+function getRows() {
+  return Array.from(document.getElementsByClassName("row"));
+}
+
+function up() {
+  let rows = getRows();
+  deselect(rows[selected]);
+  selected = (selected - 3 + totalCells) % totalCells; // Adjust for 3 columns
+  select(rows[selected]);
+}
+
+function down() {
+  let rows = getRows();
+  deselect(rows[selected]);
+  selected = (selected + 3) % totalCells; // Adjust for 3 columns
+  select(rows[selected]);
+}
+
+function left() {
+  let rows = getRows();
+  deselect(rows[selected]);
+  selected = (selected - 1 + totalCells) % totalCells;
+  select(rows[selected]);
+}
+
+function right() {
+  let rows = getRows();
+  deselect(rows[selected]);
+  selected = (selected + 1) % totalCells;
+  select(rows[selected]);
+}
+
+function select(link) {
+  link.classList.add("selected");
+  scrollToMiddle(link);
+  console.log("Selected: " + link.innerHTML);
+}
+
+function deselect(link) {
+  link.classList.remove("selected");
+}
+
+function scrollToMiddle(link) {
+  link.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "center",
+  });
+}
+
+async function toggleVideo() {
+  const currentState = await senza.lifecycle.getState();
+  if (
+    currentState == "background" ||
+    currentState == "inTransitionToBackground"
+  ) {
+    senza.lifecycle.moveToForeground();
+  } else {
+    await playVideo(videos[selected].url);
+  }
+}
+
+async function playVideo(url) {
+  try {
+    await senza.remotePlayer.load(url);
+  } catch (error) {
+    console.log("Couldn't load remote player.");
+  }
+  senza.remotePlayer.play();
+}
---
+++ videos.js
@@ -0,0 +1,42 @@
+const videos = [
+  {
+    id: "simulator",
+    thumb: "thumbs/simulator.jpg",
+    url: "https://senza-developer.s3.amazonaws.com/streams/simulator/simulator.mpd",
+    title: "Media",
+    description:
+      "This video tutorial shows how you can use the device simulator to preview how your app will run on a physical cloud connector device.",
+  },
+  {
+    id: "debugger",
+    thumb: "thumbs/debugger.jpg",
+    url: "https://senza-developer.s3.amazonaws.com/streams/debugger/debugger.mpd",
+    title: "Finance",
+    description:
+      "This video tutorial shows how you can use the Remote Debugger to interact with your cloud connector device and debug your web app. You'll also learn how to use the debugger to run a sample app by changing the URL location.",
+  },
+  {
+    id: "sample-app",
+    thumb: "thumbs/sample-app.jpg",
+    url: "https://senza-developer.s3.amazonaws.com/streams/sample-app/sample-app.mpd",
+    title: "Gaming",
+    description:
+      "This video tutorial shows how to write an app that plays two videos, one picture-in-picture and the other fullscreen, with animations to switch them back and forth.",
+  },
+  {
+    id: "zoom",
+    thumb: "thumbs/zoom.jpg",
+    url: "https://senza-developer.s3.amazonaws.com/streams/zoom/zoom.mpd",
+    title: "Music",
+    description:
+      "This video tutorial shows how to write an app that plays two videos, one picture-in-picture and the other fullscreen, with animations to switch them back and forth.",
+  },
+  {
+    id: "flags",
+    thumb: "thumbs/flags.jpg",
+    url: "https://senza-developer.s3.amazonaws.com/streams/flags/flags.mpd",
+    title: "Multi-viewer",
+    description:
+      "This video tutorial shows how to geolocate the viewer’s cloud connector by getting its IP address from the Senza client library. Warning: contains terrible fake accents.",
+  },
+];
