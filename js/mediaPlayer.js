34170ba28825527bbe42ca8667c2130b794ee395
virangaj <virangapasindu4@gmail.com>
2024-11-14 22:42:24 +0530

    add vast tag

--- js/mediaPlayer.js
+++ js/mediaPlayer.js
@@ -1,13 +1,20 @@
-import { fetchSeriesDetailsByStreamKey } from "./mediaService.js";
+import {
+  fetchSeriesDetailsByStreamKey,
+  fetchVideoDetailsBySlug,
+  fetchSeriesDetailsBySlug,
+} from "./mediaService.js";
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const urlParams = new URLSearchParams(window.location.search);
-
+  const movieSlug = urlParams.get("movie-slug");
+  const seriesSlug = urlParams.get("series-slug");
   // Extract the video source from the parameters
   const src = urlParams.get("src");
   const movieSrc = urlParams.get("movie-src");
   const seriesSrc = urlParams.get("series-src");
   const ppvSrc = urlParams.get("ppv-src");
+
+  let videoDetails = null;
 
   if (window.remotePlayer) {
     try {
@@ -16,6 +23,31 @@ document.addEventListener("DOMContentLoaded", function () {
       console.warn("Failed to unload SDK player:", error);
     }
   }
+  //  save add details
+  let vastUrl = null;
+  let poster = null;
+  let vastEnabled = null;
+  let vastPreroll = null;
+  let vastDuration = null;
+
+  if (movieSlug) {
+    videoDetails = await fetchVideoDetailsBySlug(movieSlug);
+    console.log("movie", videoDetails);
+  } else if (seriesSlug) {
+    videoDetails = await fetchSeriesDetailsBySlug(seriesSlug);
+    console.log("series", videoDetails);
+  }
+
+  if (videoDetails != null) {
+    // vastUrl = videoDetails.vast_tag_url;
+    vastUrl =
+      "https://ayotising.com/fc.php?script=rmVideo&zoneid=59&format=vast3 ";
+    poster = videoDetails.poster;
+    vastEnabled = true;
+    vastPreroll = videoDetails.pre_ad;
+    vastDuration = videoDetails.duration;
+  }
+  console.log(vastUrl, poster, vastEnabled, vastPreroll, vastDuration);
 
   const sdkVideoElement = document.getElementById("sdk-video-element");
   if (sdkVideoElement) {
@@ -33,13 +65,97 @@ document.addEventListener("DOMContentLoaded", function () {
       fullscreenToggle: false, // Hide the fullscreen button (optional)
     },
   });
-
+  var vastTagPreroll = vastUrl;
+  var vastTagMidroll = vastUrl;
+  var vastTagPostroll = vastUrl;
+  var prerollTriggered = vastPreroll;
+  var postrollTriggered = ppvSrc ? false : true;
+  var midrollRequested = false;
+  var midrollInterval = vastDuration * 60;
+  var lastMidrollTime = 0;
   function initializePlayer(videoSrc) {
     console.log("Initializing player with video source:", videoSrc);
-    player.src({
-      src: videoSrc,
-      type: "application/x-mpegURL",
-    });
+    if (vastEnabled && !ppvSrc) {
+      //console.log("Video.js player initialized");
+
+      if (!prerollTriggered) {
+        player.src(videoSrc);
+        // console.log('if', prerollTriggered);
+        player.ima({
+          adTagUrl: "",
+          showControlsForAds: true,
+          debug: false,
+        });
+        player.play();
+      } else {
+        // console.log('Else', prerollTriggered);
+        player.ima({
+          adTagUrl: vastTagPreroll,
+          showControlsForAds: true,
+          debug: false,
+        });
+        player.src(videoSrc);
+        player.play();
+      }
+      //console.log('IMA settings configured');
+      player.ima.initializeAdDisplayContainer();
+      //console.log('IMA ad display container initialized');
+      function requestMidrollAd() {
+        midrollRequested = true;
+        player.ima.changeAdTag(vastTagMidroll);
+        player.ima.requestAds();
+      }
+      player.on("timeupdate", function () {
+        var currentTime = player.currentTime();
+        //console.log('Current time:', currentTime);
+        var timeSinceLastMidroll = currentTime - lastMidrollTime;
+        //console.log(timeSinceLastMidroll);
+        if (timeSinceLastMidroll >= midrollInterval && !midrollRequested) {
+          lastMidrollTime = currentTime; // Update the last mid-roll ad time
+          ///console.log('Midroll triggered');
+          requestMidrollAd();
+        }
+      });
+      player.on("ended", function () {
+        //console.log('Video ended');
+        if (!postrollTriggered) {
+          postrollTriggered = true;
+          //console.log('Postroll triggered');
+          player.ima.requestAds({
+            adTagUrl: vastTagPostroll,
+          });
+          //console.log('Postroll ads requested');
+        }
+      });
+      player.on("adsready", function () {
+        if (midrollRequested) {
+          //console.log('Ads ready - midroll');
+        } else {
+          //console.log('Ads ready - preroll');
+          player.src(videoSrc);
+          player.play();
+        }
+      });
+      player.on("aderror", function () {
+        //console.log('Ads aderror');
+        player.play();
+      });
+      player.on("adend", function () {
+        if (lastMidrollTime > 0) {
+          //console.log('A midroll ad has finished playing.');
+          midrollRequested = false;
+        } else {
+          //console.log('The preroll ad has finished playing.');
+          prerollTriggered = true;
+        }
+        //console.log('ad ended');
+        player.play();
+      });
+    } else {
+      player.src(videoSrc);
+      player.poster(poster);
+      player.play();
+    }
   }
 
   // Determine the actual video source to use
@@ -121,43 +237,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Handle Keyboard Shortcuts
   document.addEventListener("keydown", function (event) {
-    switch (event.key) {
-      case "Enter":
-        event.preventDefault();
-        player.paused() ? player.play() : player.pause();
-        break;
-      case "ArrowRight":
-        event.preventDefault();
-        player.currentTime(player.currentTime() + 10);
-        break;
-      case "ArrowLeft":
-        event.preventDefault();
-        player.currentTime(player.currentTime() - 10);
-        break;
-      case "ArrowUp":
-        event.preventDefault();
-        player.volume(Math.min(player.volume() + 0.1, 1));
-        break;
-      case "ArrowDown":
-        event.preventDefault();
-        player.volume(Math.max(player.volume() - 0.1, 0));
-        break;
-      case "m":
-        event.preventDefault();
-        player.muted(!player.muted());
-        break;
-      case "f":
-        event.preventDefault();
-        player.requestFullscreen();
-        break;
-      case "Escape":
-        event.preventDefault();
-        if (player.isFullscreen()) {
-          player.exitFullscreen();
-        } else {
-          window.history.back();
-        }
-        break;
+    if (!midrollRequested && !postrollTriggered) {
+      switch (event.key) {
+        case "Enter":
+          event.preventDefault();
+          player.paused() ? player.play() : player.pause();
+          break;
+        case "ArrowRight":
+          event.preventDefault();
+          player.currentTime(player.currentTime() + 10);
+          break;
+        case "ArrowLeft":
+          event.preventDefault();
+          player.currentTime(player.currentTime() - 10);
+          break;
+        case "ArrowUp":
+          event.preventDefault();
+          player.volume(Math.min(player.volume() + 0.1, 1));
+          break;
+        case "ArrowDown":
+          event.preventDefault();
+          player.volume(Math.max(player.volume() - 0.1, 0));
+          break;
+        case "m":
+          event.preventDefault();
+          player.muted(!player.muted());
+          break;
+        case "f":
+          event.preventDefault();
+          player.requestFullscreen();
+          break;
+        case "Escape":
+          event.preventDefault();
+          if (player.isFullscreen()) {
+            player.exitFullscreen();
+          } else {
+            window.history.back();
+          }
+          break;
+      }
     }
   });
 });
