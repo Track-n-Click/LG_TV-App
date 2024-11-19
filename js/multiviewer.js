cab1b485ba8f8f563dfca010fbbc37c6d52f2d3d
HarshaGunathilake <harshagunathilaka5@gmail.com>
2024-11-19 13:37:27 +0530

    change the live tv order

--- js/multiviewer.js
+++ js/multiviewer.js
@@ -169,22 +169,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const videoData = {
   "row0": [
-    { "id": "vpleft", "title": "MILITARY TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/MilitaryChannel.m3u8", "type": "hls" },
-    { "id": "vpright", "title": "AYO MUSIC", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AYO.m3u8", "type": "hls" }
+    { "id": "vpleft", "title": "AYOZAT TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AyozatTV.m3u8", "type": "hls" },
+    { "id": "vpright", "title": "AYOSPORT", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AYOSPORT.m3u8 ", "type": "hls" }
   ],
   "row1": [
-    { "id": "vp00", "title": "SERENDIB TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/SerendibTV.m3u8", "type": "hls" },
-    { "id": "vp01", "title": "Ultimate Strength Network", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/Strongman.m3u8", "type": "hls" },
-    { "id": "vp02", "title": "RMT TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/RMTTV.m3u8", "type": "hls" },
-    { "id": "vp03", "title": "AYOZAT FIGHT NETWORK", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AFN.m3u8", "type": "hls" }
+    { "id": "vp00", "title": "AYO MUSIC", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AYO.m3u8 ", "type": "hls" },
+    { "id": "vp01", "title": "RMT TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/RMTTV.m3u8", "type": "hls" },
+    { "id": "vp02", "title": "AYOZAT FIGHT NETWORK", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AFN.m3u8", "type": "hls" },
+    { "id": "vp03", "title": "SERENDIB TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/SerendibTV.m3u8", "type": "hls" }
   ],
   "row2": [
     { "id": "vp10", "title": "Nub TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/NubTV.m3u8", "type": "hls" },
     { "id": "vp11", "title": "Collectables Guru", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/CollectableGurus.m3u8", "type": "hls" },
-    { "id": "vp12", "title": "AYOZAT TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AyozatTV.m3u8", "type": "hls" },
-    { "id": "vp13", "title": "AYO SPORT", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/AYOSPORT.m3u8", "type": "hls" }
+    { "id": "vp12", "title": "MILITARY TV", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/MilitaryChannel.m3u8", "type": "hls" },
+    { "id": "vp13", "title": "Ultimate Strength Network", "manifest": "https://cdn.rtmp1.vodhosting.com/hls/Strongman.m3u8", "type": "hls" }
   ]
-
 };
   // Function to initialize video.js players
   function initializeVideos() {
